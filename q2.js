0639eb12b09212e392e1e7be203400aa7cd409aa
silviors <36211503+silviors@users.noreply.github.com>
2018-11-20 17:48:36 -0200

    s

--- q2.js
+++ q2.js
@@ -237,15 +237,15 @@ function AR18(){
 function AR19(){
     var et19 = document.getElementById("et19").innerHTML;
     if(et19 === "0"){
-		senha = prompt("SENHA:");
-     if (senha === ".lp9") {
         document.getElementById("et19").innerHTML="1";
         document.getElementById("rl19").innerHTML="<div class='rele19_ligado'></div>";
-	document.getElementById("bt19").innerHTML="<a href='/?19d' class='bt19'>DESLIGA</a>";}
+	document.getElementById("bt19").innerHTML="<a href='/?19d' class='bt19'>DESLIGA</a>";
     } else {
+		senha = prompt("SENHA:");
+     if (senha === ".lp9") {
         document.getElementById("et19").innerHTML="0";
         document.getElementById("rl19").innerHTML="<div class='rele19_desligado'></div>";
-        document.getElementById("bt19").innerHTML="<a href='/?19l' class='bt19'>LIGA</a>";
+        document.getElementById("bt19").innerHTML="<a href='/?19l' class='bt19'>LIGA</a>";}
     }
 }
 
